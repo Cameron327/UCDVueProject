4f3eba6d0966d962019432f772ba39635098aaf0
Cameron <yeec327@gmail.com>
2022-03-11 16:42:50 -0800

    Finished displaying items of title and display names

---
+++ vite-project/src/App.vue
@@ -0,0 +1,41 @@
+<script>
+import Item from './components/Item.vue'
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      returned: null
+    };
+  },
+  async created() {
+    // Simple GET request using axios
+    const response = await axios.get("https://aggiefeed.ucdavis.edu/api/v1/activity/public?l=25");
+
+    console.log("Response Data: ", response.data)
+    this.returned = response.data;
+  },
+  
+  components: {
+    Item
+  }
+};
+</script>
+
+<template>
+  <Item v-for="UCDevent in returned" :title="`${UCDevent.title}`" :actorDisplayName="`${UCDevent.actor.displayName}`"/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
+// Endpoint: https://aggiefeed.ucdavis.edu/api/v1/activity/public?l=25
---
+++ vite-project/src/components/Item.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      text: "This is a test"
+    }
+  },
+  props: ['title', 'actorDisplayName']
+}
+</script>
+
+<template>
+  <div class="card text-center m-3">
+    <div class="card-body">Title of Event: {{title}}</div>
+    <div class="card-body">Display Name: {{actorDisplayName}}</div>
+  </div>
+</template>

04a4c18290ae45007236cf90700fec5a1773580a
Cameron <yeec327@gmail.com>
2022-03-11 17:22:39 -0800

    Successfully passed all props to children

--- vite-project/src/components/Item.vue
+++ vite-project/src/components/Item.vue
@@ -1,17 +1,34 @@
 <script>
+import SubList from './SubList.vue';
+
 export default {
   data() {
     return {
-      text: "This is a test"
+      show: false
     }
   },
-  props: ['UCDevent', 'title', 'actorDisplayName']
-}
+
+  props: ['title', 'actorDisplayName', 'objectType', 'published'],
+
+  methods: {
+    clickedHandler(message, e){
+      if (e) {
+        e.preventDefault()
+      }
+      console.log(message);
+    }
+  },
+
+  components: {
+    SubList
+  }
+};
 </script>
 
 <template>
-  <div class="card text-center m-3">
+  <div @click="(e) => clickedHandler('Clicked', e)"  class="card text-center m-3">
     <div class="card-body">Title of Event: {{title}}</div>
     <div class="card-body">Display Name: {{actorDisplayName}}</div>
   </div>
+  <SubList :title="`${title}`" :actorDisplayName="`${actorDisplayName}`" :objectType="`${objectType}`" :published="`${published}`"/>
 </template>
---
+++ vite-project/src/components/SubList.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+  data() {
+    return {
+      show: false
+    }
+  },
+  props: ['title', 'actorDisplayName', 'objectType', 'published'],
+}
+</script>
+
+<template>
+  <div class="card text-center m-3">
+    <div class="card-body">Title of Event: {{title}}</div>
+    <div class="card-body">Display Name: {{actorDisplayName}}</div>
+    <div class="card-body">Object Type: {{objectType}}</div>
+    <div class="card-body">Published: {{published}}</div>
+  </div>
+</template>
